df396e990610ccf8563138af995ae28b985a77fa
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2024-03-27 20:22:25 +0200

    Add Router and base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/commons/Layout";
+import Home from "./pages/Home";
+
 export default function App() {
-  return <>App</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
