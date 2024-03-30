0f8dcf1575320a44803f2d39a1ec4b1f5ce86b64
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2024-03-31 00:24:08 +0200

    Revert changes and add fallback image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
+
 import { ImageContainer } from "./styled";
+
+import fallbackImage from "../../assets/images/fallbackImage.png";
 
 export default function Image({ icon, alt }: { icon: string; alt: string }) {
   const [image, setImage] = useState(
-    `https://openaaweathermap.org/img/wn/${icon}@2x.png`
+    `https://openweathermap.org/img/wn/${icon}@2x.png`
   );
 
   function handleImageError() {
-    const imagePath = `${window.location.origin}/public/fallbackImage.png`;
-    setImage(imagePath);
+    setImage(fallbackImage);
   }
 
   return <ImageContainer onError={handleImageError} src={image} alt={alt} />;
