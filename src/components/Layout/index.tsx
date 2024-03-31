dc73290740af9cf8e9a24143ffa2ed83a79cd3ca
Aleksandar Peev <62713012+alexpeev9@users.noreply.github.com>
2024-03-31 04:23:26 +0300

    Reduce components

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom";
-import Header from "../Header";
-import { Main } from "./styled";
+import { Link, Outlet } from "react-router-dom";
+import { HeaderContainer, HeaderTitle, Main } from "./styled";
 import useScrollToTop from "../../hooks/useScrollToTop";
+import { textHeader } from "../../utils/constants";
+import { UnitForm } from "../UnitForm";
 
 export default function Layout() {
   useScrollToTop();
   return (
     <>
-      <Header />
+      <HeaderContainer>
+        <Link to="/">
+          <HeaderTitle data-test={"site-title"}>{textHeader.title}</HeaderTitle>
+        </Link>
+        <UnitForm />
+      </HeaderContainer>
       <Main>
         <Outlet />
       </Main>
